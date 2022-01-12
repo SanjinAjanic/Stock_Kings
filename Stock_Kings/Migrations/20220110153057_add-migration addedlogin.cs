837e66dff5dfd589fb0fc16d7b0248afc259e35b
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2022-01-12 22:36:15 +0100

    Add project files.

---
+++ Stock_Kings/Migrations/20220110153057_add-migration addedlogin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock_Kings.Migrations
+{
+    public partial class addmigrationaddedlogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
